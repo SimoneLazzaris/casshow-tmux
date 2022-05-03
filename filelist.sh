5b48a666082a820dff7b1a66b604f247773276e3
Simone Lazzaris <slazzaris@gmail.com>
2022-05-03 02:59:47 -0500

    Fixed nnn program detection

--- filelist.sh
+++ filelist.sh
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if command -v nnn &> /dev/null
+if command -v nnn > /dev/null
 then
     nnn
 else
